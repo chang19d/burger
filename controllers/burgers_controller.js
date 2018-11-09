0637a48f547a66c32f30fdbe736e37e1f957bd99
DChang <chang19d@gmail.com>
2018-11-08 23:39:46 -0500

    changed variable name

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,18 +3,18 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
-var cat = require("../models/burger");
+var burger = require("../models/burger");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/api/burgers", function(req, res) {
-  cat.all(function(data) {
+  burger.all(function(data) {
     console.log(data);
     res.json(data);
   });
 });
 
 router.post("/api/burgers", function(req, res) {
-  cat.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+  burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
@@ -25,7 +25,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  cat.update({ devoured: req.body.devoured }, condition, function(result) {
+  burger.update({ devoured: req.body.devoured }, condition, function(result) {
     if (result.changedRows === 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
